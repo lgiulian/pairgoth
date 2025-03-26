4eeb7728b74d8129fc1647ba2df3422deaec6bdc
Lungu Iulian <iuliancl2000@yahoo.com>
2025-03-26 13:18:24 +0200

    Rename the android app from My Application to Pairgothoid
    Rename the Android app from "My Application" to "Pairgothoid".
    
    * Change the content of `android/.idea/.name` from "My Application" to "Pairgothoid".
    * Update `android/app/src/main/AndroidManifest.xml`:
      - Change the `android:label` attribute in the `<application>` tag to `@string/app_name`.
      - Change the `android:theme` attribute in the `<application>` tag to `@style/Theme.Pairgothoid`.
      - Change the `android:label` attribute in the `<activity>` tag to `@string/app_name`.
      - Change the `android:theme` attribute in the `<activity>` tags to `@style/Theme.Pairgothoid`.
    * Modify `android/app/src/main/java/com/iqoid/pairgoth/MainActivity.kt`:
      - Change the `MyApplicationTheme` to `PairgothoidTheme` in the `onCreate` method.
      - Change the `MyApplicationTheme` to `PairgothoidTheme` in the `GreetingPreview` composable function.
    * Update `android/app/src/main/java/com/iqoid/pairgoth/ui/theme/Theme.kt`:
      - Change the `MyApplicationTheme` function name to `PairgothoidTheme`.
    * Change the value of the `app_name` string in `android/app/src/main/res/values/strings.xml` to "Pairgothoid".
    * Update `android/settings.gradle.kts`:
      - Change the `rootProject.name` value from "My Application" to "Pairgothoid".
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/lgiulian/pairgoth?shareId=XXXX-XXXX-XXXX-XXXX).

--- android/app/src/main/java/com/iqoid/pairgoth/MainActivity.kt
+++ android/app/src/main/java/com/iqoid/pairgoth/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.iqoid.pairgoth.ui.theme.MyApplicationTheme
+import com.iqoid.pairgoth.ui.theme.PairgothoidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
+            PairgothoidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MyApplicationTheme {
+    PairgothoidTheme {
         Greeting("Android")
     }
 }
